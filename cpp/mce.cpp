f8ef1840a78738b56ba02a7fcd921ef7c9092dc4
David Halls <dahalls@gmail.com>
2019-01-11 00:00:41 +0000

    Add more C++ version

--- cpp/mce.cpp
+++ cpp/mce.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 #include <utility>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <boost/any.hpp>
 #include "json.hpp"
 
@@ -31,10 +33,37 @@ typedef std::function<std::shared_ptr<any>(std::shared_ptr<any>)> map_fn;
 typedef std::function<std::shared_ptr<any>(cmap_table&,
                                            std::shared_ptr<any>,
                                            std::shared_ptr<any>)> set_entry_fn;
-typedef std::function<std::shared_ptr<any>(std::shared_ptr<any>)> lambda;
+typedef std::shared_ptr<any> function(std::shared_ptr<any>);
+typedef std::function<function> lambda;
+
+std::shared_ptr<any> make_symbol(const std::string& s) {
+    return std::make_shared<any>(std::make_shared<symbol>(s));
+}
+
+std::shared_ptr<any> make_string(const std::string& s) {
+    return std::make_shared<any>(std::make_shared<std::string>(s));
+}
 
 std::shared_ptr<any> cons(std::shared_ptr<any> car, std::shared_ptr<any> cdr) {
     return std::make_shared<any>(std::make_shared<pair>(car, cdr));
+}
+
+std::shared_ptr<any> list_ref(std::shared_ptr<any> l, size_t i) {
+    while (i > 0) {
+        l = any_cast<std::shared_ptr<pair>>(*l)->second;
+        --i;
+    }
+
+    return any_cast<std::shared_ptr<pair>>(*l)->first;
+}
+
+std::shared_ptr<any> list_rest(std::shared_ptr<any> l, size_t i) {
+    while (i > 0) {
+        l = any_cast<std::shared_ptr<pair>>(*l)->second;
+        --i;
+    }
+
+    return any_cast<std::shared_ptr<pair>>(*l)->second;
 }
 
 std::shared_ptr<any> list_to_vector(std::shared_ptr<any> l) {
@@ -112,38 +141,332 @@ bool is_yield_defn(std::shared_ptr<any> args) {
     if (args->type() != typeid(std::shared_ptr<pair>)) {
         return false;
     }
-    auto p = any_cast<std::shared_ptr<pair>>(*args);
-    return (p->first->type() == typeid(symbol)) &&
-           (any_cast<symbol>(*p->first) == "MCE-YIELD-DEFINITION");
+    auto first = list_ref(args, 0);
+    return (first->type() == typeid(std::shared_ptr<symbol>)) &&
+           (*any_cast<std::shared_ptr<symbol>>(*first) ==
+            "MCE-YIELD-DEFINITION");
 }
 
 std::shared_ptr<any> memoize_lambda(lambda proc, std::shared_ptr<any> defn) {
-    return std::make_shared<any>(
+    return std::make_shared<any>(lambda(
         [proc, defn](std::shared_ptr<any> args) -> std::shared_ptr<any> {
             if (is_yield_defn(args)) {
                 return defn;
             }
             return proc(args);
-        });
+        }));
 }
 
-std::vector<lambda> forms;
+std::shared_ptr<any> memoize_lambda(std::shared_ptr<any> proc,
+                                    std::shared_ptr<any> defn) {
+    return memoize_lambda(any_cast<lambda>(*proc), defn);
+}
+
+std::shared_ptr<any> send(std::shared_ptr<any> k, std::shared_ptr<any> v) {
+    return cons(k, v);
+}
+
+std::shared_ptr<any> ctenv_lookup(std::shared_ptr<any> i,
+                                  std::shared_ptr<any> env) {
+    auto ip = any_cast<std::shared_ptr<pair>>(*i);
+    auto first = any_cast<double>(*ip->first);
+    auto second = any_cast<double>(*ip->second);
+    auto bindings = any_cast<std::shared_ptr<pair>>(*list_ref(env, first));
+    auto v = any_cast<std::shared_ptr<vector>>(*bindings->second);
+    return (*v)[second];
+}
+
+std::shared_ptr<any> symbol_lookup(std::shared_ptr<any> args) {
+    auto i = list_ref(args, 1);
+    return std::make_shared<any>(lambda(
+        [i](std::shared_ptr<any> args) -> std::shared_ptr<any> {
+            auto k = list_ref(args, 0);
+            auto env = list_ref(args, 1);
+            return send(k, ctenv_lookup(i, env));
+        }));
+}
+
+std::shared_ptr<any> send_value(std::shared_ptr<any> args) {
+    auto exp = list_ref(args, 1);
+    return std::make_shared<any>(lambda(
+        [exp](std::shared_ptr<any> args) -> std::shared_ptr<any> {
+            auto k = list_ref(args, 0);
+            return send(k, exp);
+        }));
+}
+
+std::shared_ptr<any> make_step_contn(std::shared_ptr<any> k,
+                                     std::shared_ptr<any> args) {
+    return cons(make_symbol("MCE-STEP_CONTN"), cons(k, args));
+}
+
+bool is_step_contn(std::shared_ptr<any> args) {
+    if (args->type() != typeid(std::shared_ptr<pair>)) {
+        return false;
+    }
+    auto first = list_ref(args, 0);
+    return (first->type() == typeid(std::shared_ptr<symbol>)) &&
+           (*any_cast<std::shared_ptr<symbol>>(*first) ==
+            "MCE-STEP-CONTN");
+}
+
+std::shared_ptr<any> step_contn_k(std::shared_ptr<any> args) {
+    return list_ref(args, 1);
+}
+
+std::shared_ptr<any> step_contn_args(std::shared_ptr<any> args) {
+    return list_rest(args, 1);
+}
+
+bool is_transfer(std::shared_ptr<any> args) {
+    if (args->type() != typeid(std::shared_ptr<pair>)) {
+        return false;
+    }
+    auto first = list_ref(args, 0);
+    return (first->type() == typeid(std::shared_ptr<symbol>)) &&
+           (*any_cast<std::shared_ptr<symbol>>(*first) ==
+            "MCE-TRANSFER");
+}
+
+std::shared_ptr<any> transfer_args(std::shared_ptr<any> args) {
+    return list_rest(args, 0);
+}
+
+std::shared_ptr<any> transfer(std::shared_ptr<any> args) {
+    auto k = list_ref(args, 0);
+    auto fn = list_ref(args, 2);
+    return any_cast<lambda>(*fn)(make_step_contn(k,
+        cons(make_symbol("MCE-TRANSFER"), list_rest(args, 2))));
+}
+
+std::shared_ptr<any> make_global_env() {
+    auto bindings = cons(std::make_shared<any>(),
+                         std::make_shared<any>(std::make_shared<vector>()));
+    return cons(bindings, std::make_shared<any>());
+}
+
+std::shared_ptr<any> make_env_args(std::shared_ptr<any> env,
+                                   std::shared_ptr<any> args) {
+    return cons(make_symbol("MCE-ENV-ARGS"), cons(env, args));
+}
+
+bool is_env_args(std::shared_ptr<any> args) {
+    if (args->type() != typeid(std::shared_ptr<pair>)) {
+        return false;
+    }
+    auto first = list_ref(args, 0);
+    return (first->type() == typeid(std::shared_ptr<symbol>)) &&
+           (*any_cast<std::shared_ptr<symbol>>(*first) ==
+            "MCE-ENV-ARGS");
+}
+
+std::shared_ptr<any> env_args_env(std::shared_ptr<any> args) {
+    return is_env_args(args) ? list_ref(args, 1) : make_global_env();
+}
+
+std::shared_ptr<any> env_args_args(std::shared_ptr<any> args) {
+    return is_env_args(args) ? list_rest(args, 1) : args;
+}
+
+std::shared_ptr<any> result(std::shared_ptr<any> exp) {
+    auto a = std::make_shared<any>(std::make_shared<vector>());
+    auto v = any_cast<std::shared_ptr<vector>>(*a);
+    v->push_back(make_symbol("MCE-RESULT"));
+    v->push_back(exp);
+    return a;
+}
+
+bool is_result(std::shared_ptr<any> exp) {
+    if (exp->type() != typeid(std::shared_ptr<vector>)) {
+        return false;
+    }
+    auto v = any_cast<std::shared_ptr<vector>>(*exp);
+    return (v->size() == 2) &&
+           ((*v)[0]->type() == typeid(std::shared_ptr<symbol>)) &&
+           (*any_cast<std::shared_ptr<symbol>>(*(*v)[0]) == "MCE-RESULT");
+}
+
+std::shared_ptr<any> result_val(std::shared_ptr<any> exp) {
+    return (*any_cast<std::shared_ptr<vector>>(*exp))[1];
+}
+
+std::unordered_map<std::string, function*> global_table {
+    "result", result
+};
+
+std::unordered_set<function*> kenvfn_table;
+
+std::shared_ptr<any> find_global(const symbol& sym) {
+    return std::make_shared<any>(lambda(global_table.at(sym)));
+}
+
+std::shared_ptr<any> step(std::shared_ptr<any> state) {
+    return any_cast<lambda>(*list_ref(state, 0))(list_rest(state, 1));
+}
+
+std::shared_ptr<any> run(std::shared_ptr<any> state) {
+    while (!is_result(state)) {
+        state = step(state);
+    }
+
+    return result_val(state);
+}
+
+std::shared_ptr<any> globalize(std::shared_ptr<any> x,
+                               std::shared_ptr<any> args,
+                               std::shared_ptr<any> cf);
+
+std::shared_ptr<any> handle_global_lambda(std::shared_ptr<any> args,
+                                          std::shared_ptr<any> fn,
+                                          std::shared_ptr<any> cf) {
+    auto f = any_cast<lambda>(*fn);
+
+    if (is_step_contn(args)) {
+        auto sck = step_contn_k(args);
+        auto sca = step_contn_args(args);
+        if (is_transfer(sca)) {
+            return f(make_step_contn(sck, transfer_args(sca)));
+        }
+        auto eaa = env_args_args(sca);
+        return send(sck, globalize(f(eaa), eaa, cf));
+    }
+
+    auto eaa = env_args_args(args);
+    return globalize(f(eaa), eaa, cf);
+}
+
+std::shared_ptr<any> lookup_global(const symbol& sym);
+
+std::shared_ptr<any> handle_global_lambda_kenv(std::shared_ptr<any> args,
+                                               std::shared_ptr<any> fn) {
+    auto f = any_cast<lambda>(*fn);
+
+    if (is_step_contn(args)) {
+        auto sca = step_contn_args(args);
+        return f(cons(step_contn_k(args),
+                      cons(env_args_env(sca),
+                           env_args_args(sca))));
+    }
+
+    return run(f(cons(lookup_global(symbol("result")),
+                      cons(env_args_env(args),
+                           env_args_args(args)))));
+}
+
+std::shared_ptr<any> wrap_global_lambda(std::shared_ptr<any> fn,
+                                        std::shared_ptr<any> cf) {
+    const lambda l = any_cast<lambda>(fn);
+    auto p = l.target<function*>();
+
+    if (kenvfn_table.find(*p) != kenvfn_table.end()) {
+        return std::make_shared<any>(lambda(
+            [fn](std::shared_ptr<any> args) -> std::shared_ptr<any> {
+                return handle_global_lambda_kenv(args, fn);
+            }));
+    }
+
+    return std::make_shared<any>(lambda(
+        [fn, cf](std::shared_ptr<any> args) -> std::shared_ptr<any> {
+            return handle_global_lambda(args, fn, cf);
+        }));
+}
+
+std::shared_ptr<any> lookup(symbol& sym, std::shared_ptr<any> env) {
+    while (!env->empty()) {
+        auto ep = any_cast<std::shared_ptr<pair>>(*env);
+        auto bindings = any_cast<std::shared_ptr<pair>>(*ep->first);
+        auto values = any_cast<std::shared_ptr<vector>>(*bindings->second);
+        auto syms = bindings->first;
+        size_t i = 0;
+
+        while (!syms->empty() && (i < values->size())) {
+            auto sp = any_cast<std::shared_ptr<pair>>(*syms);
+            if (*any_cast<std::shared_ptr<symbol>>(*sp->first) == sym) {
+                return (*values)[i];
+            }
+            syms = sp->second;
+            ++i;
+        }
+
+        env = ep->second;
+    }
+
+    return lookup_global(sym);
+}
+
+std::shared_ptr<any> runtime_lookup(std::shared_ptr<any> args) {
+    auto name = any_cast<std::shared_ptr<symbol>>(*list_ref(args, 1));
+    return std::make_shared<any>(lambda(
+        [name](std::shared_ptr<any> args) -> std::shared_ptr<any> {
+            auto k = list_ref(args, 0);
+            auto env = list_ref(args, 1);
+            return send(k, lookup(*name, env));
+        }));
+}
+
+#define define_forms(...) \
+std::vector<lambda> forms { \
+    __VA_ARGS__ \
+}; \
+enum class forms { \
+    __VA_ARGS__ \
+};
+
+define_forms(
+    symbol_lookup,
+    send_value,
+    runtime_lookup
+)
+
+std::shared_ptr<any> lookup_global(const symbol& sym) {
+    auto r = find_global(sym);
+    auto defn = cons(std::make_shared<any>(
+                    static_cast<double>(forms::global_lambda)),
+                cons(make_symbol(sym),
+                std::make_shared<any>()));
+    auto f = std::make_shared<any>();
+    auto f2 = memoize_lambda(
+        [f](std::shared_ptr<any> args) -> std::shared_ptr<any> {
+            return any_cast<lambda>(*f)(args);
+        },
+        defn);
+    *f = *memoize_lambda(wrap_global_lambda(r, f2), defn);
+    return f;
+}
+
+std::shared_ptr<any> globalize(std::shared_ptr<any> x,
+                               std::shared_ptr<any> args,
+                               std::shared_ptr<any> cf) {
+    if (x->type() != typeid(lambda)) {
+        return x;
+    }
+
+    auto defn = cons(std::make_shared<any>(
+                    static_cast<double>(forms::constructed_function)),
+                cons(args,
+                cons(cf,
+                std::make_shared<any>())));
+    auto f = std::make_shared<any>();
+    auto f2 = memoize_lambda(
+        [f](std::shared_ptr<any> args) -> std::shared_ptr<any> {
+            return any_cast<lambda>(*f)(args);
+        },
+        defn);
+    *f = *memoize_lambda(wrap_global_lambda(x, f2), defn);
+    return f;
+}
 
 std::shared_ptr<any> make_form(std::shared_ptr<any> n,
                                std::shared_ptr<any> args) {
     auto defn = cons(n, args);
 
-    std::shared_ptr<any> f;
-    std::shared_ptr<any> f2 = memoize_lambda(
+    auto f = std::make_shared<any>();
+    auto f2 = memoize_lambda(
         [f](std::shared_ptr<any> args) -> std::shared_ptr<any> {
-            return any_cast<lambda>(f)(args);
+            return any_cast<lambda>(*f)(args);
         },
         defn);
-    f = memoize_lambda(
-        [n, f2](std::shared_ptr<any> args) -> std::shared_ptr<any> {
-            return forms[any_cast<double>(n)](cons(f2, args));
-        },
-        defn);
+    *f = *memoize_lambda(forms[any_cast<double>(*n)](cons(f2, args)), defn);
 
     return f;
 }
@@ -155,8 +478,12 @@ std::shared_ptr<any> make_form(std::shared_ptr<any> args) {
 
 bool is_unmemoized(std::shared_ptr<vector> v) {
     return (v->size() == 2) &&
-           ((*v)[0]->type() == typeid(symbol)) &&
-           (any_cast<symbol>(*(*v)[0]) == "MCE-UNMEMOIZED");
+           ((*v)[0]->type() == typeid(std::shared_ptr<symbol>)) &&
+           (*any_cast<std::shared_ptr<symbol>>(*(*v)[0]) == "MCE-UNMEMOIZED");
+}
+
+std::shared_ptr<any> unmemoized_repexp(std::shared_ptr<vector> v) {
+    return (*v)[1];
 }
 
 std::shared_ptr<any> memoize_aux(std::shared_ptr<any> exp,
@@ -170,16 +497,14 @@ std::shared_ptr<any> memoize_aux(std::shared_ptr<any> exp,
 
     if (type == typeid(std::shared_ptr<vector>)) {
         auto v = any_cast<std::shared_ptr<vector>>(*exp);
-        if ((v->size() == 2) &&
-            ((*v)[0]->type() == typeid(symbol)) &&
-            (any_cast<symbol>(*(*v)[0]) == "MCE-UNMEMOIZED")) {
-            auto repexp = (*v)[1];
-            auto r = std::make_shared<any>(symbol("unspecified"));
+        if (is_unmemoized(v)) {
+            auto repexp = unmemoized_repexp(v);
+            auto r = make_symbol("unspecified");
             auto entry = table_set(tab, exp, memoize_lambda(
                 [r](std::shared_ptr<any> args) -> std::shared_ptr<any> {
                     return any_cast<lambda>(*r)(args);
                 }, repexp));
-            (*r) = make_form(fn(repexp));
+            *r = *make_form(fn(repexp));
             return r;
         }
         return vector_cmap(fn, exp, tab, table_set);
@@ -190,10 +515,20 @@ std::shared_ptr<any> memoize_aux(std::shared_ptr<any> exp,
 
 std::shared_ptr<any> memoize(std::shared_ptr<any> exp) {
     cmap_table tab;
-    map_fn f = [&tab,&f](std::shared_ptr<any> x)->std::shared_ptr<any> {
-        return memoize_aux(x, tab, f);
+    map_fn fn = [&tab, &fn](std::shared_ptr<any> x) -> std::shared_ptr<any> {
+        return memoize_aux(x, tab, fn);
     };
-    return f(exp);
+    return fn(exp);
+}
+
+bool is_serialized(std::shared_ptr<vector> v) {
+    return (v->size() == 2) &&
+           ((*v)[0]->type() == typeid(std::shared_ptr<symbol>)) &&
+           (*any_cast<std::shared_ptr<symbol>>(*(*v)[0]) == "MCE-SERIALIZED");
+}
+
+std::shared_ptr<any> serialized_n(std::shared_ptr<vector> v) {
+    return (*v)[1];
 }
 
 std::shared_ptr<any> deserialize_aux(std::shared_ptr<any> exp,
@@ -206,21 +541,30 @@ std::shared_ptr<any> deserialize_aux(std::shared_ptr<any> exp,
         return cmap(fn, exp, tab, set_entry);
     }
 
+    if (type == typeid(std::shared_ptr<vector>)) {
+        auto v = any_cast<std::shared_ptr<vector>>(*exp);
+        if (is_serialized(v)) {
+            return tab[serialized_n(v)];
+        }
+        return vector_cmap(fn, exp, tab, set_entry);
+    }
+
     return exp;
 }
 
 std::shared_ptr<any> deserialize(std::shared_ptr<any> exp) {
-    unsigned int counter = 0;
+    size_t counter = 0;
     cmap_table tab;
-    set_entry_fn set_entry = [](cmap_table& tab,
-                                std::shared_ptr<any> v,
-                                std::shared_ptr<any> entry) {
-        table_set(tab, std::make_shared<any>(counter++), entry);
+    set_entry_fn set_entry = [&counter](cmap_table& tab,
+                                        std::shared_ptr<any> v,
+                                        std::shared_ptr<any> entry) {
+        return table_set(tab, std::make_shared<any>(counter++), entry);
     };
-    map_fn f= [](std::shared_ptr<any> x)->std::shared_ptr<any> {
-        return deserialize_aux(x, tab, fn, set_entry);
-    };
-    return f(exp);
+    map_fn fn = [&tab, &fn, &set_entry]
+        (std::shared_ptr<any> x) -> std::shared_ptr<any> {
+            return deserialize_aux(x, tab, fn, set_entry);
+        };
+    return fn(exp);
 }
 
 std::shared_ptr<any> unpickle_aux(const json& j) {
@@ -232,9 +576,9 @@ std::shared_ptr<any> unpickle_aux(const json& j) {
     case char_code:
         return std::make_shared<any>(j[1].get<std::string>()[0]);
     case string_code:
-        return std::make_shared<any>(j[1].get<std::string>());
+        return make_string(j[1].get<std::string>());
     case symbol_code:
-        return std::make_shared<any>(symbol(j[1].get<std::string>()));
+        return make_symbol(j[1].get<std::string>());
     case pair_code:
         return cons(unpickle_aux(j[1]), unpickle_aux(j[2]));
     case vector_code:
