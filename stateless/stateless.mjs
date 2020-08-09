040c1a9699b36da31092254096073795e3c37a3e
David Halls <dahalls@gmail.com>
2020-08-09 18:27:08 +0100

    Fix wrong var name

--- stateless/stateless.mjs
+++ stateless/stateless.mjs
@@ -4,15 +4,14 @@ import sodium_plus from 'sodium-plus';
 const { CryptographyKey, SodiumPlus } = sodium_plus;
 
 export async function handler(event) {
-    const key64 = process.env.STATELESS_KEY;
-    const key = new CryptographyKey(Buffer.from(key64, 'base64'));
-    const sodium = await SodiumPlus.auto();
-
     const form = qs.parse(event.body);
     const state = JSON.parse(form.state);
     const msg = Buffer.from(state.msg, 'base64');
     const mac = Buffer.from(state.mac, 'base64');
-   
+
+    const key64 = process.env.STATELESS_KEY;
+    const key = new CryptographyKey(Buffer.from(key64, 'base64'));
+    const sodium = await SodiumPlus.auto();
     if (!await sodium.crypto_auth_verify(msg, key, mac)) {
         throw new Error('failed to verify state');
     }
@@ -27,6 +26,6 @@ export async function handler(event) {
         headers: {
             'Content-Type': 'text/html'
         },
-        body: await run(data, key64, args)
+        body: await run(msg, key64, args)
     };
 }
