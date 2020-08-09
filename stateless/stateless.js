65849400a628a0fb1aaea3bc9b000e1852732dd2
David Halls <dahalls@gmail.com>
2020-08-09 08:11:57 +0100

    Put require() hack back in

--- stateless/stateless.js
+++ stateless/stateless.js
@@ -1,3 +1,8 @@
+// This require is to get Netlify to include stateless.mjs in the deployment
+try {
+    require('./stateless.mjs');
+} catch {}
+
 exports.handler = async event => {
     return await (await import('./stateless.mjs')).handler(event);
 };
