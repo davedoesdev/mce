dad44baf303f74462ee4f3ddcc9dd7e84cf3e215
David Halls <dahalls@gmail.com>
2020-08-09 08:21:41 +0100

    Netlify: Add jsdom in the require hacks

--- stateless/stateless.js
+++ stateless/stateless.js
@@ -1,19 +1,10 @@
 // These requires are to get Netlify to include the .mjs files in the deployment
-try {
-    require('./stateless.mjs');
-} catch {}
-try {
-    require('./run.mjs');
-} catch {}
-try {
-    require('./shtml.mjs');
-} catch {}
-try {
-    require('sodium-plus');
-} catch {}
-try {
-    require('@davedoesdev/mce');
-} catch {}
+try { require('./stateless.mjs'); } catch {}
+try { require('./run.mjs'); } catch {}
+try { require('./shtml.mjs'); } catch {}
+try { require('@davedoesdev/mce'); } catch {}
+try { require('sodium-plus'); } catch {}
+try { require('jsdom'); } catch {}
 
 exports.handler = async event => {
     return await (await import('./stateless.mjs')).handler(event);
