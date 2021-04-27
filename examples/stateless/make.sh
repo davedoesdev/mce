d256749f826dfadbb33a135da9ae213977354349
David Halls <dahalls@gmail.com>
2021-04-27 06:11:41 +0100

    Really fix counter URL!

--- examples/stateless/make.sh
+++ examples/stateless/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-url="$(echo '(display-binary (save "./netlify/functions/stateless"))' | ../../scm/expand | ../../scm/scan | ../../scm/mce)"
+url="$(echo '(display-binary (save "/.netlify/functions/stateless"))' | ../../scm/expand | ../../scm/scan | ../../scm/mce)"
 ../../scm/expand < counter.scm | ../../scm/scan | STATELESS_KEY="$(cat ../../stateless/key)" node ../../stateless/initial.mjs --config "url=$url" > ../../stateless/counter.html
