515d937ea6ca2a2d5b1faf6120091527824df5d0
David Halls <dahalls@gmail.com>
2021-04-27 07:51:02 +0100

    Put back netlify bundle hacks

--- stateless/stateless.js
+++ stateless/stateless.js
@@ -1,6 +1,13 @@
-// This is for local testing only (netlify dev).
-// netlify dev doesn't support ES modules but the Netlify site does.
+// These requires are to get Netlify to include them in the deployment
+try { require('./stateless-function.mjs'); } catch {}
+try { require('./run.mjs'); } catch {}
+try { require('./shtml.mjs'); } catch {}
+try { require('@davedoesdev/mce'); } catch {}
+try { require('sodium-plus'); } catch {}
+try { require('jsdom'); } catch {}
 
+// This is for local testing (netlify dev) but gets used in deployment too.
+// netlify dev doesn't support ES modules but the Netlify site does.
 exports.handler = async event => {
     return await (await import('./stateless-function.mjs')).handler(event);
 };
