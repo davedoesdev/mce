22ced2255b5d3aa5a30a66486b4464cba12c574c
David Halls <dahalls@gmail.com>
2021-04-27 07:57:50 +0100

    Move netlify function into separate directory

--- stateless/functions/stateless.js
+++ stateless/functions/stateless.js
@@ -1,7 +1,7 @@
 // These requires are to get Netlify to include them in the deployment
-try { require('./stateless-function.mjs'); } catch {}
-try { require('./run.mjs'); } catch {}
-try { require('./shtml.mjs'); } catch {}
+try { require('../stateless.mjs'); } catch {}
+try { require('../run.mjs'); } catch {}
+try { require('../shtml.mjs'); } catch {}
 try { require('@davedoesdev/mce'); } catch {}
 try { require('sodium-plus'); } catch {}
 try { require('jsdom'); } catch {}
@@ -9,5 +9,5 @@ try { require('jsdom'); } catch {}
 // This is for local testing (netlify dev) but gets used in deployment too.
 // netlify dev doesn't support ES modules but the Netlify site does.
 exports.handler = async event => {
-    return await (await import('./stateless-function.mjs')).handler(event);
+    return await (await import('../stateless.mjs')).handler(event);
 };
